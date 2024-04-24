90d1b0c28d6cd9703336690876e861e2c39279a8
FireflyWE <wissalelkhettab@gmail.com>
2024-04-24 01:59:34 +0100

    commit

--- src/main/java/ma/emsi/hopital/HopitalApplication.java
+++ src/main/java/ma/emsi/hopital/HopitalApplication.java
@@ -2,10 +2,17 @@ package ma.emsi.hopital;
 
 import ma.emsi.hopital.entities.Patient;
 import ma.emsi.hopital.repository.PatientRepository;
+import ma.emsi.hopital.security.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
 import java.util.Date;
 
@@ -26,11 +33,11 @@ public class HopitalApplication implements CommandLineRunner {
 		patient1.setNom("Yamcha");
 		patient1.setDateNaissance(new Date());
 		patient1.setMalade(true);
-		patient1.setScore(50);
+		patient1.setScore(500);
 		patientRepository.save(patient1);
 
 		//method ajout 2 with args
-		Patient patient2=new Patient(null,"krillin",new Date(),false,20);
+		Patient patient2=new Patient(null,"krillin",new Date(),false,200);
 		patientRepository.save(patient2);
 
 		//method ajout 3 en utilisant Builder
@@ -41,5 +48,23 @@ public class HopitalApplication implements CommandLineRunner {
 				.score(110)
 				.build();
 		patientRepository.save(patient3);
+	}
+	@Bean
+	CommandLineRunner commandLineRunner(JdbcUserDetailsManager jdbcUserDetailsManager){
+		PasswordEncoder passwordEncoder = passwordEncoder();
+		return args -> {
+				jdbcUserDetailsManager.createUser(
+					User.withUsername("user12").password(passwordEncoder.encode("1234")).roles("USER").build());
+				jdbcUserDetailsManager.createUser(
+					User.withUsername("user22").password(passwordEncoder.encode("1234")).roles("USER").build());
+				jdbcUserDetailsManager.createUser(
+					User.withUsername("admin2").password(passwordEncoder.encode("1234")).roles("ADMIN","USER").build());
+
+		};
+	}
+
+	@Bean
+	PasswordEncoder passwordEncoder(){
+		return new BCryptPasswordEncoder();
 	}
 }
--- src/main/java/ma/emsi/hopital/web/PatientController.java
+++ src/main/java/ma/emsi/hopital/web/PatientController.java
@@ -1,6 +1,7 @@
 package ma.emsi.hopital.web;
 
 
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import ma.emsi.hopital.entities.Patient;
 import ma.emsi.hopital.repository.PatientRepository;
@@ -9,7 +10,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -17,7 +20,7 @@ import java.util.List;
 @Controller  @AllArgsConstructor
 public class PatientController {
     private PatientRepository patientRepository;
-    @GetMapping("/index")
+    @GetMapping("/user/index")
     public String index(Model model, @RequestParam(name="page",defaultValue = "0")int p,@RequestParam(name="size",defaultValue = "3") int s,
                         @RequestParam(name = "keyword",defaultValue = "")String kw){
 
@@ -28,10 +31,38 @@ public class PatientController {
         model.addAttribute("keyword",kw);
         return "patients";
     }
-    @GetMapping("/delete")
+    @GetMapping("/admin/delete")
     public String delete (Long id,String keyword,int page){
         patientRepository.deleteById(id);
+        return "redirect:/user/index?page="+page+"&keyword="+keyword;
+    }
+    @GetMapping("/admin/formPatients")
+    public String formPatient(Model model){
+        model.addAttribute("patient",new Patient());
+        return "formPatients";
+    }
+    @PostMapping(path = "/admin/save")
+    public String save(Model model, @Valid Patient patient, BindingResult bindingResult,
+                       @RequestParam(defaultValue = "0") int page,
+                       @RequestParam(defaultValue = "") String keyword){
+        if(bindingResult.hasErrors()) return "formPatients";
+        patientRepository.save(patient);
         return "redirect:/index?page="+page+"&keyword="+keyword;
+    }
+
+    @GetMapping("/admin/editPatient")
+    public String editPatient(Model model,Long id,String keyword,int page){
+        Patient patient=patientRepository.findById(id).orElse(null);
+        if(patient==null) throw new RuntimeException("patient introuvable");
+        model.addAttribute("patient",patient);
+        model.addAttribute("keyword",keyword);
+        model.addAttribute("page",page);
+        return "editPatient";
+    }
+    @GetMapping("/")
+
+    public String home(){
+        return "redirect:/user/index";
     }
 
 
